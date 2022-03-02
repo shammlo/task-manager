e738c513196bde7d717cb5a36882a636e113bc3a
sham <shamlooo@yahoo.com>
2022-03-02 23:01:01 +0300

    Init commit - production

--- src/database/mongoose.ts
+++ src/database/mongoose.ts
@@ -4,35 +4,8 @@ const mongoose = require('mongoose');
 // ----------------------------------------------------------------
 // * MongoDB URL and Database name
 
-mongoose.connect(process.env.MONGO_DB_URL + '/' + process.env.MONGO_DB_NAME, {
+mongoose.connect(process.env.MONGO_DB_URL, {
     useNewUrlParser: true,
 });
 
 // ----------------------------------------------------------------
-// - Task
-
-// const Task = mongoose.model('Tasks', {
-//     description: {
-//         type: String,
-//         trim: true,
-//         required: true,
-//     },
-//     completed: {
-//         type: Boolean,
-//         default: false,
-//     },
-// });
-
-// const myTask = new Task({
-//     description: 'Read your book',
-//     completed: false,
-// });
-
-// myTask
-//     .save()
-//     .then(() => {
-//         console.log(myTask);
-//     })
-//     .catch((error: Error) => {
-//         console.log(error);
-//     });
